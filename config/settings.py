ca062e231f461f4eb4afb6e40b0aadba4fe94098
retsushi1309 <{marktwo1216@icloud.com}>
2025-08-06 03:28:35 +0900

    update4

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3af9w2@@i16d-aap8oj-ycu3f#0@!5!wojj4d6%6q8xczvp*)^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['praxisweb.jp', 'www.praxisweb.jp', '160.251.181.156']
+ALLOWED_HOSTS = ['praxisweb.jp', 'localhost', 'www.praxisweb.jp', '160.251.181.156']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
